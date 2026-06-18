1f1112bfd33e0176118b8c6f83fa54dd26cb5339
karthikeyanm-dev <karthikeyan.m@itechpanda.com>
2026-06-18 12:21:23 +0530

    feat:test api added

---
+++ src/main/java/com/karthi/testspring/Controller.java
@@ -0,0 +1,25 @@
+package com.karthi.testspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+    @GetMapping
+    public String testApi(){
+        return "API is working";
+    }
+
+    @GetMapping("/id/{id}")
+    public String testApi2(@PathVariable String id){
+        return "API is working with ID : " +id;
+    }
+
+}
