60c0ce92681ae04f1081d33d011dbd0023adb730
karthikeyanm-dev <karthikeyan.m@itechpanda.com>
2026-06-18 12:28:17 +0530

    feat:say my name feature added

--- src/main/java/com/karthi/testspring/Controller.java
+++ src/main/java/com/karthi/testspring/Controller.java
@@ -2,6 +2,7 @@ package com.karthi.testspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +21,11 @@ public class Controller {
     @GetMapping("/id/{id}")
     public String testApi2(@PathVariable String id){
         return "API is working with ID : " +id;
+    }
+
+    @GetMapping("/say")
+    public String testApi3(@RequestParam String name){
+        return "Hi  : "+name;
     }
 
 }
